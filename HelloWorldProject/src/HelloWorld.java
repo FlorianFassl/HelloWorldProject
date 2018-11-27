298f660a0e127bfb7956604a775cca4c83120c5b
florian.fassl <florian.fassl@KK-GYM-EDV1-07.gym.kk.local>
2018-11-27 17:39:57 +0100

    zweiter commit 1.473 Kommentare hinzugefügt

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,8 +3,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Klasse");
-
+		System.out.println("Hello Klasse");
+//das ist ein kommentar, kein code der ausgeführt wird.
+//das ist ein einzeiliger kommentar
+		/*
+		 * Mehrzeiliger Kommentar schreiben
+		 */
 	}
 
 }
